54f1f4794f8385f9faea5a3e845d93c666c5fcd8
sngsuNoh <s000715@naver.com>
2026-01-14 17:17:25 +0900

    solved

---
+++ Lv.1/모든 레코드 조회하기.sql
@@ -0,0 +1,11 @@
+-- 문제 제목: 모든 레코드 조회하기 
+-- 문제 난이도: Lv.1
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- ---
+
+SELECT
+    *
+FROM
+    ANIMAL_INS
+ORDER BY 
+    ANIMAL_ID ASC;
