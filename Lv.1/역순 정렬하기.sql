c8581ba30cd93d221d9a77edd36caad67d09584d
sngsuNoh <s000715@naver.com>
2026-01-15 01:28:13 +0900

    solved

---
+++ Lv.1/역순 정렬하기.sql
@@ -0,0 +1,11 @@
+-- 문제 제목: 역순 정렬하기
+-- 문제 난이도: Lv.1
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59035
+-- ---
+
+SELECT
+    NAME, DATETIME
+FROM
+    ANIMAL_INS
+ORDER BY 
+    ANIMAL_ID DESC;
